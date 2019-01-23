50172ad9f46aaf2a63df017d5c52fbc14f60eb35
daniviki <daningerviki@gmail.com>
2019-01-23 12:32:52 +0100

    add excercises 6-8.

---
+++ week02/day3/src/FourRectangles.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class FourRectangles {
+
+    public static void mainDraw(Graphics graphics){
+        // draw four different size and color rectangles.
+        // avoid code duplication.
+
+        int x = 10;
+        int y = 10;
+
+
+        for( int i = 0; i < 4; i++ ) {
+            graphics.setColor(new Color(100,0, 100 + (30 * i)));
+            graphics.fillRect(x + (60*i), y, 30 + (12*i), 15 + (12*i));
+
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
