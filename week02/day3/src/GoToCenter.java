dc7b702445a6b067cc7f85deda6e2233f1c86233
daniviki <daningerviki@gmail.com>
2019-01-23 11:45:13 +0100

    add exercise 1-5.

---
+++ week02/day3/src/GoToCenter.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class GoToCenter {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a line from that point to the center of the canvas.
+        // Draw 3 lines with that function. Use loop for that.
+
+        graphics.setColor(Color.BLUE);
+
+        int x1 = 50;
+        int y1 = 50;
+
+        for( int i = 0; i < 3; i++ ) {
+            drawOneLine(x1 += 10,y1, graphics);
+
+        }
+
+
+    }
+
+    public static void drawOneLine(int x1, int y1, Graphics graphics) {
+        int c = 160;
+        graphics.drawLine(x1, y1, c, c);
+    }
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
---
+++ week02/day3/src/HorizontalLines.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class HorizontalLines {
+    public static void mainDraw(Graphics graphics) {
+        // Create a line drawing function that takes 3 parameters:
+        // The x and y coordinates of the line's starting point and the graphics
+        // and draws a 50 long horizontal line from that point.
+        // Draw 3 lines with that function. Use loop for that.
+
+        graphics.setColor(Color.BLUE);
+
+        int x1 = 50;
+        int y1 = 50;
+
+        for( int i = 0; i < 3; i++ ) {
+            drawOneLine(x1 , y1 += 10, graphics);
+        }
+    }
+
+    public static void drawOneLine(int x1, int y1, Graphics graphics) {
+        int xlength = x1 + 50;
+        graphics.drawLine(x1, y1, xlength, y1);
+    }
+
+
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
