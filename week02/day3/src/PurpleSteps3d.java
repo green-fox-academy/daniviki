4e4271150de85ae3878f7b3e8716c08a9edebfbf
daniviki <daningerviki@gmail.com>
2019-01-23 17:08:27 +0100

    not done

--- week02/day3/src/PurpleSteps3d.java
+++ week02/day3/src/PurpleSteps3d.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class PurpleSteps3d {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps-3d/r4.png]
+
+        int xy = 0;
+        int size = 10;
+        graphics.setColor(new Color(164, 66, 244));
+        for( int i = 0; i < 6; i++ ) {
+            size +=10;
+           graphics.fillRect(xy + (i * size - size),xy +(i * size - size),size ,size);
+           //graphics.fillRect(xy + (i + bigger), xy + (i + bigger), size ,size);
+
+        }
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
 }
--- week02/day3/src/RainbowBoxFunction.java
+++ week02/day3/src/RainbowBoxFunction.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class RainbowBoxFunction {
+    public static void mainDraw(Graphics graphics) {
+        // Create a square drawing function that takes 3 parameters:
+        // The square size, the fill color, graphics
+        // and draws a square of that size and color to the center of the canvas.
+        // Create a loop that fills the canvas with rainbow colored squares.
+
+
+        int size = 40;
+        int color = 255;
+
+        for( int i = 0; i < 10; i++ ) {
+            drawOneLine(size * (i+1) , color - (i * 20), graphics);
+        }
+    }
+
+    public static void drawOneLine(int size, int color, Graphics graphics) {
+        int center = 160;
+        graphics.setColor(new Color (0, 0,color));
+        graphics.drawRect(center - (size/2), center - (size/2), size, size);
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
 }
