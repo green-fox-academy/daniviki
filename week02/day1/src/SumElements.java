0d612160cfe1a3e87ead02ddd65a85cbad13c113
daniviki <daningerviki@gmail.com>
2019-01-25 12:26:39 +0100

    correction

--- week02/day1/src/SumElements.java
+++ week02/day1/src/SumElements.java
@@ -1,19 +1,8 @@
 public class SumElements {
     public static void main(String[] args) {
 
-        int lines = 5;
-        for( int i = 0; i <= 5; i++ ) {
-            for (int j = 0; j <= 5-i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k <= i; k++) {
-                System.out.print(" *");
-            }
-
-            System.out.println();
-        }
-
-
+        int[] r = {54, 23, 66, 12};
+        System.out.println( r[1] + r[2]);
 
     }
 }
