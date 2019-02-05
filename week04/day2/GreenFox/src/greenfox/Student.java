4400b672b83bd64baa86672e6b78961b6e17a142
daniviki <daningerviki@gmail.com>
2019-02-05 12:54:53 +0100

    finish Student class

--- week04/day2/GreenFox/src/greenfox/Student.java
+++ week04/day2/GreenFox/src/greenfox/Student.java
@@ -1,4 +1,36 @@
 package greenfox;
 
-public class Student {
+public class Student extends Person {
+
+    String previousOrganization;
+    int skippedDays;
+
+    public Student (String name, int age, String gender, String previousOrganization) {
+        super(name,age,gender);
+        this.previousOrganization = previousOrganization;
+        skippedDays = 0;
+    }
+
+    public Student () {
+        previousOrganization = "The School of Life";
+        skippedDays = 0;
+    }
+
+
+
+
+    public void getGoal() {
+        System.out.println("Be a junior software developer.");
+    }
+    public void introduce () {
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " +
+                gender + " from " + previousOrganization + " who skipped " + skippedDays +
+                " days from the course already.");
+    }
+    public void skipDays (int numberOfDays) {
+
+        skippedDays = skippedDays + numberOfDays;
+
+    }
+
 }
