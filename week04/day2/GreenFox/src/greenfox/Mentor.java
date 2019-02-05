645ed79c183836f2595e66ec761f866adc38e78b
daniviki <daningerviki@gmail.com>
2019-02-05 13:04:02 +0100

    finish Mentor class

--- week04/day2/GreenFox/src/greenfox/Mentor.java
+++ week04/day2/GreenFox/src/greenfox/Mentor.java
@@ -1,4 +1,24 @@
 package greenfox;
 
-public class Mentor {
+public class Mentor extends Person{
+
+    String level;
+
+    public Mentor (String name, int age, String gender, String level) {
+        super(name,age,gender);
+        level = level;
+    }
+    public Mentor () {
+        level = "intermediate";
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " +
+                gender + level + " mentor.");
+    }
+
+    public void getGoal() {
+        System.out.println("Educate brilliant junior software developers.");
+    }
+
 }
