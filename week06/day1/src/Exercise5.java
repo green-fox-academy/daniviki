6458b89ac814f8d3b65b0177088f3c9c251b8db3
daniviki <daningerviki@gmail.com>
2019-02-25 13:17:19 +0100

    finish Exercise 5

---
+++ week06/day1/src/Exercise5.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2);
+
+        int sumOfOddNumbers = numbers.stream()
+                .filter(number -> number % 2 != 0)
+                .mapToInt(number -> number).sum();
+        System.out.println(sumOfOddNumbers);
+    }
+}
