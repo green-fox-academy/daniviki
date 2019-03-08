45e115e986b05b0d2ebca9dd5ed597f9ba6c8201
daniviki <daningerviki@gmail.com>
2019-03-08 12:26:30 +0100

    finish exercise: Login page

--- week07/day5/src/main/java/com/greenfoxacademy/foxclub/controller/MainController.java
+++ week07/day5/src/main/java/com/greenfoxacademy/foxclub/controller/MainController.java
@@ -1,4 +1,34 @@
 package com.greenfoxacademy.foxclub.controller;
 
+import com.greenfoxacademy.foxclub.model.Fox;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class MainController {
+
+    @RequestMapping("/")
+    public String home(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("food", "pizza and lemonade" );
+        model.addAttribute("tricks", 2);
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginForm() {
+       return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLoginForm(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("food", "bread and water" );
+        model.addAttribute("tricks", 3);
+        return "index";
+    }
 }
